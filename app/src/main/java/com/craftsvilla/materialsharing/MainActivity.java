123d90a03b3865d96d2491b2e95d97485a9f6921
Naresh-Narsing <nareshnarsing38@gmail.com>
2016-04-20 12:10:59 +0530

    Material Sharing Example

---
+++ app/src/main/java/com/craftsvilla/materialsharing/MainActivity.java
@@ -0,0 +1,156 @@
+package com.craftsvilla.materialsharing;
+
+import android.animation.Animator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    ImageButton imageButton;
+    RelativeLayout revealView, layoutButtons;
+    Animation alphaAnimation;
+    float pixelDensity;
+    boolean flag = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        pixelDensity = getResources().getDisplayMetrics().density;
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageButton = (ImageButton) findViewById(R.id.launchTwitterAnimation);
+        revealView = (RelativeLayout) findViewById(R.id.linearView);
+        layoutButtons = (RelativeLayout) findViewById(R.id.layoutButtons);
+
+        alphaAnimation = AnimationUtils.loadAnimation(this, R.anim.alpha_anim);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchTwitter(v);
+            }
+        });
+    }
+
+    public void launchTwitter(View view) {
+        int x = imageView.getRight();
+        int y = imageView.getBottom();
+        x = (int) (x - ((28 * pixelDensity) + (16 * pixelDensity)));
+
+        int hypotenuse = (int) Math.hypot(imageView.getWidth(), imageView.getHeight());
+
+        if (flag) {
+            imageButton.setBackgroundResource(R.drawable.rounded_cancel_button);
+            imageButton.setImageResource(R.drawable.image_cancel);
+
+            FrameLayout.LayoutParams parameters = (FrameLayout.LayoutParams)
+                    revealView.getLayoutParams();
+            parameters.height = imageView.getHeight();
+            revealView.setLayoutParams(parameters);
+
+            Animator anim = ViewAnimationUtils.createCircularReveal(revealView, x, y, 0, hypotenuse);
+            anim.setDuration(700);
+
+            anim.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    layoutButtons.setVisibility(View.VISIBLE);
+                    layoutButtons.startAnimation(alphaAnimation);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+
+            revealView.setVisibility(View.VISIBLE);
+            anim.start();
+
+            flag = false;
+        } else {
+
+            imageButton.setBackgroundResource(R.drawable.rounded_button);
+            imageButton.setImageResource(R.drawable.twitter_logo);
+
+            Animator anim = ViewAnimationUtils.createCircularReveal(revealView, x, y, hypotenuse, 0);
+            anim.setDuration(400);
+
+            anim.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    revealView.setVisibility(View.GONE);
+                    layoutButtons.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+
+            anim.start();
+            flag = true;
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
